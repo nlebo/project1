4b54c224474e2771c0813eb9bce25e3606c9dddd
LeeWan <etlyc2@naver.com>
2020-10-24 21:18:27 +0900

    CartMoveSpeed Improve
    CartMoveSpeed Improve

--- Assets/Script/RidingObject/Cart.cs
+++ Assets/Script/RidingObject/Cart.cs
@@ -5,27 +5,34 @@ using UnityEngine;
 public class Cart : MonoBehaviour
 {
     bool EngineOn;
+    
 
 
     public float MoveSpeed;
+    public float MaxMoveSpeed;
+    public float AddSpeedPerSecond;
     // Start is called before the first frame update
     void Start()
     {
-        
+        MoveSpeed = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(EngineOn)
+        if(EngineOn){
+
+            MoveSpeed += AddSpeedPerSecond * Time.deltaTime;
+            if(MoveSpeed > MaxMoveSpeed) MoveSpeed = MaxMoveSpeed;
             transform.Translate(Vector3.left * Time.deltaTime * MoveSpeed);
-        
+        }
     }
 
     public bool PushEngine()
     {
 
         EngineOn = !EngineOn;
+        if(!EngineOn) MoveSpeed = 0;
         return EngineOn;
     }
 }
