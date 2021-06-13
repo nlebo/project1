3c3e1d2fa0b9a0a2e848749cf203dfaf43ff5e7b
Wan Lee <61404266+nlebo@users.noreply.github.com>
2021-06-13 14:02:52 +0900

    [클라이언트][이완]
    - 새로운 씬 및 캐릭터 스크립트 재정의
    - 캐릭터 무브 및 화면전환 / 애니메이션 연결

---
+++ Assets/NewScene/Script/Character/CharacterMove.cs
@@ -0,0 +1,183 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : IEMove
+{
+	enum AnimState
+	{
+		Idle = 0,
+		Walk = 1,
+		Jump = 3,
+	}
+	Rigidbody MoveObject;
+	Transform TF_Foots;
+	Animator Anim;
+	bool IsJump = false;
+	bool JumpHigh = false;
+	bool IsJumping = false;
+
+	private void Start()
+	{
+		EventManager.Instance.AddFixedUpdateManager(UpdateManager);
+	}
+
+	public override void SetSpeed(float _speed)
+	{
+		speed = _speed;
+	}
+
+	public override void SetObject(object obj)
+	{
+		MoveObject = (Rigidbody)obj;
+		TF_Foots = MoveObject.transform.Find("Foots");
+		Anim = MoveObject.GetComponent<Animator>();
+	}
+
+	void UpdateManager()
+	{
+		if (MoveObject == null)
+			return;
+
+		CheckGround();
+		if (IsJump) IsJump = false;
+
+		MoveObject.AddForce(Dic * speed, ForceMode.Impulse);
+		
+
+	}
+
+	public override void Move()
+	{
+		if (IsJump)
+			return;
+
+		Dic = GetDirection().normalized;
+	}
+
+	public override void Jump()
+	{
+		if (IsJump || !InputManager.GetKeyDown(KeyCode.Space) || IsJumping)
+			return;
+
+		Dic = GetDirection().normalized;
+		Dic += Vector3.up;
+		Dic.Normalize();
+
+		IsJump = true;
+		IsJumping = true;
+	}
+
+	Vector3 GetDirection()
+	{
+		Vector3 _dic = Vector3.zero;
+
+		if (InputManager.GetKeyIn(KeyCode.W))
+			_dic.z += 1;
+		if (InputManager.GetKeyIn(KeyCode.S))
+			_dic.z -= 1;
+		if (InputManager.GetKeyIn(KeyCode.A))
+			_dic.x -= 1;
+		if (InputManager.GetKeyIn(KeyCode.D))
+			_dic.x += 1;
+
+		return _dic;
+	}
+
+	bool CheckGround()
+	{
+		RaycastHit[] hit;
+		hit = Physics.RaycastAll(TF_Foots.position, -Vector3.up);
+
+		foreach (var value in hit)
+		{
+			if (value.transform.tag == "Plane")
+			{
+				if (Vector3.SqrMagnitude(TF_Foots.position - value.point) <= 0.01f)
+				{
+					IsJumping = false;
+
+					return true;
+				}
+				else if (Vector3.SqrMagnitude(TF_Foots.position - value.point) <= 0.5f)
+				{
+					Anim.SetInteger("JumpState", 1);
+					IsJumping = true;
+				}
+				else
+				{
+					Anim.SetInteger("JumpState", 0);
+					IsJumping = true;
+				}
+			}
+		}
+		return false;
+	}
+
+	public override bool IsMove() 
+	{
+		if (IsJumping)
+			return false;
+
+		return Dic.x != 0 || Dic.z != 0;
+	}
+
+	public override void SetAnim()
+	{
+		int state = GetNowState();
+
+		if (IsJumping || Anim.GetCurrentAnimatorStateInfo(0).normalizedTime == 0)
+		{
+			Anim.SetBool("ChangeState", false);
+		}
+		int State = Anim.GetInteger("State");
+
+		switch ((AnimState)state)
+		{
+			case AnimState.Idle:
+				{
+					Anim.SetInteger("State", state);
+
+					if (State != 0)
+					{
+						Anim.Play("Idle", 0);
+						Anim.SetBool("ChangeState", true);
+					}
+				}
+				break;
+			case AnimState.Walk:
+				{
+					Anim.SetInteger("State", state);
+					Anim.SetFloat("WalkBlend", -Dic.z);
+					if (State != 1)
+					{
+						Anim.Play("Walk", 0);
+						Anim.SetBool("ChangeState", true);
+					}
+				}
+				break;
+			case AnimState.Jump:
+				{
+					Anim.SetInteger("State", state);
+
+					if (State!=3)
+					{
+						Anim.Play("Jump", 0);
+						Anim.SetBool("ChangeState", true);
+					}
+				}
+				break;
+		}
+	}
+
+	int GetNowState()
+	{
+		if (IsJumping)
+			return (int)AnimState.Jump;
+
+		if (IsMove())
+			return (int)AnimState.Walk;
+
+		return (int)AnimState.Idle;
+	}
+}
---
+++ Assets/NewScene/Script/IEMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IEMove : MonoBehaviour
+{
+    protected float speed;
+    protected Vector3 Dic;
+
+    public virtual void Move() { }
+    public virtual void Stop() { }
+    public virtual void Jump() { }
+    public virtual void SetSpeed(float _speed) { }
+
+    public virtual void SetObject(object obj) { }
+
+    public virtual bool IsMove() { return false; }
+    public virtual void SetAnim() { }
+}
